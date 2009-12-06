6b3a4e57a23d61819ffc55ddf15d04fc9c6d30d1
mateu x hunter <hunter@missoula.org>
2009-12-06 12:50:55 -0500

    StringUtils example

---
+++ examples/StringUtilsTrial.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang.StringUtils;
+
+class StringUtilsTrial {
+  public static void main(String[] args) {
+
+    // Join all Strings in the Array into a Single String, separated by $#$
+    System.out.println("7) Join Strings using separator >>>";
+    //    + StringUtils.join(new String[] { "AB", "CD", "EF" }, "$#$"));
+  }
+}
+
+
